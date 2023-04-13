49e10a7b79aed0ca68bf706e68ff4d71e7eff78b
samuelnovaiscavelho <samuel.novais247@gmail.com>
2023-04-13 02:09:44 -0300

    Exercicio 11 concluido

--- Python Exercicios/Segunda Lista de Exercicios/Ex11.py
+++ Python Exercicios/Segunda Lista de Exercicios/Ex11.py
@@ -4,3 +4,10 @@ de aumento. Você deverá escrever um algoritmo que recebe 2 números reais repr
 os salários antes e depois do aumento e deverá calcular e exibir o percentual de aumento que
 João obteve.
 '''
+salarioPassado = float(input("Informe seu salário do passado: "))
+salarioAtual =  float(input("Informe seu salário atual: "))
+
+calcularPerce = ((salarioAtual - salarioPassado) / salarioPassado) * 100
+print("Salario de antes: %.2f"% salarioPassado)
+print("Salario Atual: %.2f"% salarioAtual)
+print("Percentual de aumento foi: %.0f"% calcularPerce,"%")
---
+++ Python Exercicios/Segunda Lista de Exercicios/Ex12.py
@@ -0,0 +1,6 @@
+'''
+O RM de um aluno da FIAP é composto por 5 dígitos. Sua tarefa é escrever um algoritmo que
+recebe um RM e retorna a somatória de todos os dígitos do RM. Por exemplo, suponha que
+o aluno tenha o RM 56395, seu algoritmo deverá imprimir como saída 28 = 5+6+3+9+5.
+Dica: realize várias divisões e restos de divisões por 10.
+'''
