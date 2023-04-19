0e77a4f69d76c1b4803a74ef46f36aa87254199b
samuelnovaiscavelho <samuel.novais247@gmail.com>
2023-04-19 00:38:41 -0300

    exercicio 02 lista 03

--- Python Exercicios/Segunda Lista de Exercicios/lista3/Ex02.py
+++ Python Exercicios/Segunda Lista de Exercicios/lista3/Ex02.py
@@ -3,3 +3,13 @@
 houve um empate.
 
 '''
+num1 = int(input("Informe um valor inteiro: "))
+num2 = int(input("Informe outro valor inteiro: "))
+
+if num1 > num2:
+    print(num1, " é maior que ", num2)
+
+elif(num1 < num2):
+    print(num2, " é maior que ", num1) 
+else:
+    print("Houve um empate pois ", num1, " é o mesmo valor que ", num2)   
