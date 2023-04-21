e8cc53a7afb3feb0c1442010e2cbb1b277252dab
samuelnovaiscavelho <samuel.novais247@gmail.com>
2023-04-20 23:41:55 -0300

    Exercicio 06 concluido

--- Python Exercicios/Segunda Lista de Exercicios/lista3/Ex06.py
+++ Python Exercicios/Segunda Lista de Exercicios/lista3/Ex06.py
@@ -1,3 +1,12 @@
 '''
 Faça um programa para ler dois números inteiros A e B e informar se A é divisível por B.
 '''
+
+numA = int(input("Informe um número inteiro A: "))
+numB = int(input("Informe o segundo númeiro inteiro B: "))
+
+if (numA % numB) == 0:
+    print("A é divisivel por B! ")
+
+else:
+    print("A não é divisivel por B! ")
---
+++ Python Exercicios/Segunda Lista de Exercicios/lista3/Ex07.py
@@ -0,0 +1,11 @@
+'''
+A raiz quadrada é uma operação que apenas aceita números positivos. Escreva um algoritmo
+que lê um número qualquer e retorna a raiz quadrada desse número se possível. Use a função
+math.sqrt(<numero>) para calcular a raiz quadrada em Python. Note que, para usar essa função,
+você terá que importar o módulo math antes.
+'''
+import math
+
+#coloque aqui o resto do seu codigo
+#tudo na frente do sustenido eh
+#considerado um comentario em Python
