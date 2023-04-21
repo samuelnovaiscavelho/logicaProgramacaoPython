6861e609f6189207a3a2e17ffa3ef110b9219376
samuelnovaiscavelho <samuel.novais247@gmail.com>
2023-04-20 23:23:52 -0300

    Exercicio 05 concluido

--- Python Exercicios/Segunda Lista de Exercicios/lista3/Ex05.py
+++ Python Exercicios/Segunda Lista de Exercicios/lista3/Ex05.py
@@ -7,7 +7,24 @@ apenas nos dias úteis, escreva um algoritmo que recebe:
 a) o total de dias úteis de um mês
 b) o total de horas trabalhadas pelo trabalhador
 c) quanto o trabalhador recebe por hora
-Calcula e mostra o valor recebido a título de hora-extra (se houver) e o salário nal do
+Calcula e mostra o valor recebido a título de hora-extra se houver e o salário final do
 trabalhador.
-
 '''
+
+totalDiasUteisMes = int(input("Informe a quantidade de dias úteis no mês: "))
+totalHorasTrabalhadasTrabalhador = int(input("Informe o total de horas trabalhadas: "))
+recebePorHora = float(input("Quanto recebe por hora trabalhada: "))
+trabalhoDiaria = 8
+cargaHorariaTrabalhada = totalDiasUteisMes * trabalhoDiaria 
+salario =  totalDiasUteisMes * (trabalhoDiaria * recebePorHora)
+if cargaHorariaTrabalhada < totalHorasTrabalhadasTrabalhador:
+    valorReceber = totalHorasTrabalhadasTrabalhador - cargaHorariaTrabalhada
+    horaExtra = recebePorHora * 1.5
+    salarioExtra = valorReceber * horaExtra
+    print("Carga de horas extras: ", valorReceber)
+    print("Salário: ", salario + salarioExtra)
+
+else:
+    print("Sem horas extras")
+    print("Salário: ", salario)
+
---
+++ Python Exercicios/Segunda Lista de Exercicios/lista3/Ex06.py
@@ -0,0 +1,3 @@
+'''
+Faça um programa para ler dois números inteiros A e B e informar se A é divisível por B.
+'''
